36a1cb8251426ca8498603c0ba85a2be3fdbab29
Alberto Florido <143635386+aflr@users.noreply.github.com>
2023-12-23 11:31:31 +0100

    Add files via upload

---
+++ so_long/mlx/Makefile_Unix.mk
@@ -0,0 +1,44 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile_Unix.mk                                   :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: aflorido <aflorido@student.42malaga.com    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/02/26 21:36:38 by W2Wizard          #+#    #+#              #
+#    Updated: 2023/12/07 22:48:26 by aflorido         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+#//= Colors =//#
+BOLD	:= \033[1m
+BLACK	:= \033[30;1m
+RED		:= \033[31;1m
+GREEN	:= \033[32;1m
+YELLOW	:= \033[33;1m
+BLUE	:= \033[34;1m
+MAGENTA	:= \033[35;1m
+CYAN	:= \033[36;1m
+WHITE	:= \033[37;1m
+RESET	:= \033[0m
+
+#//= Make Rules =//#
+$(NAME): $(OBJS)
+	@ar rc $@ $^
+	@echo "$(GREEN)$(BOLD)Done$(RESET)" 1>/dev/null
+
+%.o: %.c $(HDRS)
+	@echo "$(GREEN)$(BOLD)Compiling:$(RESET) $(notdir $<)" 1>/dev/null
+	@$(CC) $(CFLAGS) -o $@ -c $< $(HEADERS)
+
+# Convert shaders to .c files
+$(SRC_DIR)/mlx_%_shader.c: $(SHADER_DIR)/default.%
+	@echo "$(GREEN)$(BOLD)Shader to C: $< -> $@$(RESET)" 1>/dev/null
+	@bash tools/compile_shader.sh $< > $@
+
+clean:
+	@echo "$(RED)Cleaning$(RESET)" 1>/dev/null
+	@rm -f $(OBJS) $(SHDR)
+
+fclean: clean
+	@rm -f $(NAME)
